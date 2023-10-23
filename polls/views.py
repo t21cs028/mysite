ab659fcaf6e096d56794c103e4e8cbc08c051ad5
JEON HYODAM <t21cs028@yamanashi.ac.jp>
2023-10-23 13:16:25 +0900

    初めてのdjango, その他３

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,15 @@
 from django.http import HttpResponse
-from django.template import loader
+#from django.template import loader
+#from django.shortcuts import render
+#from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 
 from .models import Question
 
+    #return HttpResponse("You're looking at question %s." % question_id)
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
 
 def results(request, question_id):
@@ -17,12 +22,8 @@ def vote(request, question_id):
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("polls/index.html")
-    context = {
-        "latest_question_list": latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
-
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
 
 
 
