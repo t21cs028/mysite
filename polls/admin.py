155c6d7d3b55dfc4cbc18310473f33e36c51ee6c
JEON HYODAM <t21cs028@yamanashi.ac.jp>
2023-10-23 15:07:26 +0900

    7(1)

--- polls/admin.py
+++ polls/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 
 from .models import Question
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Date information", {"fields": ["pub_date"]}),
+        (None, {"fields": ["question_text"]}),
+    ]
+
+admin.site.register(Question, QuestionAdmin)
